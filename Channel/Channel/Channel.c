84fcffd908c9f7a1e88152195a4343a3b30b7275
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-02-26 21:16:52 +0200

    working

--- Channel/Channel/Channel.c
+++ Channel/Channel/Channel.c
@@ -15,6 +15,7 @@ int isRandomNoise = 0, numberOfFlippedBits = 0;
 double noiseProbability;
 
 void addRandomNoise() {
+    memset(&dataBuffer, 0, 26); // TODO change number
     srand(randomSeed);
     for (int i = 0; i < sizeof(dataBuffer); i++) {
         double randomDouble = (double)rand() / (double)RAND_MAX;
@@ -27,9 +28,91 @@ void addRandomNoise() {
 }
 
 void addDeterministicNoise() {
+    memset(&dataBuffer, 0, 26); // TODO change number
     for (int i = cycleLength - 1; i < sizeof(dataBuffer); i += cycleLength) {
         dataBuffer[i] = 1 - dataBuffer[i];
         numberOfFlippedBits++;
+    }
+}
+
+void senderSocketInit() {
+    // Creating sender socket for listening
+    senderListenSockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (senderListenSockfd < 0) {
+        perror("Can't create socket");
+        exit(1);
+    }
+
+    // Creating sender address struct
+    int addrSize = sizeof(senderListenSockAddr);
+    memset(&senderListenSockAddr, 0, addrSize);
+    senderListenSockAddr.sin_family = AF_INET;
+    senderListenSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    senderListenSockAddr.sin_port = htons(0); // Will be changed by bind()
+
+    // Binding sender socket to port
+    retVal = bind(senderListenSockfd, (struct sockaddr*)&senderListenSockAddr, addrSize);
+    if (retVal != 0) {
+        perror("Bind failed");
+        exit(1);
+    }
+
+    // Listening on sender listening socket
+    retVal = listen(senderListenSockfd, 1);
+    if (retVal != 0) {
+        perror("Listen to sender failed");
+        exit(1);
+    }
+
+    // Printing IP and port of sender socket
+    memset(&IPAddress, 0, 1024); // TODO change
+    if (getsockname(senderListenSockfd, (struct sockaddr*)&senderListenSockAddr, &addrSize) == 0) {
+        inet_ntop(AF_INET, &senderListenSockAddr.sin_addr, IPAddress, 1024); //TODO change number
+        printf("sender socket: IP address = %s port = %d\n", IPAddress, ntohs(senderListenSockAddr.sin_port));
+    }
+    else {
+        perror("Error at getsockname of sender socket");
+        exit(1);
+    }
+}
+
+void recieverSocketInit() {
+    // Creating reciever socket for listening
+    recieverListenSockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (recieverListenSockfd < 0) {
+        perror("Can't create socket");
+        exit(1);
+    }
+
+    // Creating reciever address struct
+    int addrSize = sizeof(recieverListenSockAddr);
+    memset(&recieverListenSockAddr, 0, addrSize);
+    recieverListenSockAddr.sin_family = AF_INET;
+    recieverListenSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    recieverListenSockAddr.sin_port = htons(0); // Will be changed by bind()
+
+    // Binding reciever socket to port
+    retVal = bind(recieverListenSockfd, (struct sockaddr*)&recieverListenSockAddr, addrSize);
+    if (retVal != 0) {
+        perror("Bind failed");
+        exit(1);
+    }
+
+    // Listening on reciever listening socket
+    retVal = listen(recieverListenSockfd, 1);
+    if (retVal != 0) {
+        perror("Listen to reciever failed");
+        exit(1);
+    }
+
+    // Printing IP and port of reciever socket
+    if (getsockname(recieverListenSockfd, (struct sockaddr*)&recieverListenSockAddr, &addrSize) == 0) {
+        inet_ntop(AF_INET, &recieverListenSockAddr.sin_addr, IPAddress, 1024); //TODO change
+        printf("reciever socket: IP address = %s port = %d\n", IPAddress, ntohs(recieverListenSockAddr.sin_port));
+    }
+    else {
+        perror("Error at getsockname of reciever socket");
+        exit(1);
     }
 }
 
@@ -57,63 +140,18 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // Creating sender socket for listening
-    senderListenSockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (senderListenSockfd < 0) {
-        perror("Can't create socket");
-        exit(1);
-    }
-
-    // Printing IP and port of sender socket
-    int addrLen = sizeof(senderListenSockAddr);
-    if (getsockname(senderListenSockfd, (struct sockaddr*)&senderListenSockAddr, &addrLen) == 0) {
-        inet_ntop(AF_INET, &senderListenSockAddr.sin_addr, IPAddress, 1024); //TODO change
-        printf("sender socket: IP address = %s port = %d\n", IPAddress, ntohs(senderListenSockAddr.sin_port));
-    }
-    else {
-        perror("Error at getsockname of sender socket");
-        exit(1);
-    }
-
-    // Creating reciever socket for listening
-    recieverListenSockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (recieverListenSockfd < 0) {
-        perror("Can't create socket");
-        exit(1);
-    }
-
-    // Printing IP and port of reciever socket
-    addrLen = sizeof(recieverListenSockAddr);
-    if (getsockname(recieverListenSockfd, (struct sockaddr*)&recieverListenSockAddr, &addrLen) == 0) {
-        inet_ntop(AF_INET, &recieverListenSockAddr.sin_addr, IPAddress, 1024); //TODO change
-        printf("reciever socket: IP address = %s port = %d\n", IPAddress, ntohs(recieverListenSockAddr.sin_port));
-    }
-    else {
-        perror("Error at getsockname of sender socket");
-        exit(1);
-    }
-
-    // Listening on sender listening socket
-    retVal = listen(senderListenSockfd, 1);
-    if (retVal != 0) {
-        perror("Listen to sender failed");
-        exit(1);
-    }
-
+    // Initilizing listen sockets
+    senderSocketInit();
+    recieverSocketInit();
+        
     // Accepting sender connection
-    addrLen = sizeof(senderConnSockAddr);
+    int addrLen = sizeof(senderConnSockAddr);
     senderConnSockfd = accept(senderListenSockfd, (struct sockaddr*)&senderConnSockAddr, &addrLen);
     if (senderConnSockfd < 0) {
         perror("Accept sender connection failed");
         exit(1);
     }
 
-    // Listening on reciever listening socket
-    retVal = listen(recieverListenSockfd, 1);
-    if (retVal != 0) {
-        perror("Listen to reciever failed");
-        exit(1);
-    }
 
     // Accepting reciever connection
     addrLen = sizeof(recieverConnSockAddr);
